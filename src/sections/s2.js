c00ab9b4edf94ee8509d246e81faa295c9a0d583
Ce Gao <magic@m9020312345.mtel.net.mo>
2019-08-02 12:36:19 +0800

    all

--- src/sections/s2.js
+++ src/sections/s2.js
@@ -8,13 +8,13 @@ export default class S2 extends React.PureComponent {
         <div className="col-1">
           <div className="p1">
             <div className="img"></div>
-            <h2>孩子吃的海参<div>购&nbsp;&nbsp;&nbsp;&nbsp;买</div></h2>
+            <h2>孩子吃的海参<div>详&nbsp;&nbsp;&nbsp;&nbsp;情</div></h2>
             <p>妈妈常常会告诉孩子越是珍贵的东西<br/>
               取之有度才是真正的体面<br/>
               不可以浪费 亦不可贪食</p>
           </div>
           <div className="p2">
-            <h2>妈妈吃的海参<div>购&nbsp;&nbsp;&nbsp;&nbsp;买</div></h2>
+            <h2>妈妈吃的海参<div>详&nbsp;&nbsp;&nbsp;&nbsp;情</div></h2>
             <p>在健康美丽的使命召唤下<br/>
               妈妈每周进行三次吃海参的仪式<br/>
               虔诚且优雅</p>
@@ -22,7 +22,7 @@ export default class S2 extends React.PureComponent {
           </div>
           <div className="p3">
             <div className="img"></div>
-            <h2>爸爸吃的海参<div>购&nbsp;&nbsp;&nbsp;&nbsp;买</div></h2>
+            <h2>爸爸吃的海参<div>详&nbsp;&nbsp;&nbsp;&nbsp;情</div></h2>
             <p>爸爸每天吃一头海参是妈妈安排的<br/>
               爸爸问为什么？<br/>
               妈妈说姥爷就是这个习惯</p>
@@ -31,7 +31,7 @@ export default class S2 extends React.PureComponent {
         <div  className="col-2">
           <div className="p4">
             <div className="img"></div>
-            <h2>爷爷奶奶吃的海参<div>购&nbsp;&nbsp;&nbsp;&nbsp;买</div></h2>
+            <h2>爷爷奶奶吃的海参<div>详&nbsp;&nbsp;&nbsp;&nbsp;情</div></h2>
             <p>爷爷奶奶每次都分食一头巨大的海参曾为<br/>
               了横着切还是竖着切吵了一次架之后<br/>
               每次都是横着切，因为奶奶吵赢了</p>
--- src/sections/s3.js
+++ src/sections/s3.js
@@ -6,27 +6,27 @@ export default class S3 extends React.PureComponent {
     return (
       <div className="s3-wrapper">
         <div className="s3-box">
-          <img src="/assets/section/3/p1.png" alt=""></img>
+          <img src="/assets/section/3/1.jpg" alt=""></img>
           <h3>孩子吃海参的建议</h3>
           <p><span></span>3岁以上的孩子适合经常使用海参，能够高效的补充营养，增强体质以及促进脑发育，建议单次食用不超过1/2头，每周2头。</p>
         </div>
         <div className="s3-box">
-        <img src="/assets/section/3/p3.png" alt=""></img>
+          <img src="/assets/section/3/2.jpg" alt=""></img>
           <h3>女士食用海参的建议</h3>
           <p><span></span>日常保养，消减疲惫，延缓衰老。建议每周3头，怀孕期间，更是可以母婴双补，用量可以适当提升。</p>
         </div>
         <div className="s3-box">
-        <img src="/assets/section/3/p4.png" alt=""></img>
+          <img src="/assets/section/3/3.jpg" alt=""></img>
           <h3>男士食用海参的建议</h3>
           <p><span></span>除了提升免疫力，抗疲劳以外海参对男士还有提升生殖功能的显著功效。所以男士日常食用每周4头左右，备孕期间可以每天一头。</p>
         </div>
         <div className="s3-box">
-        <img src="/assets/section/3/p5.png" alt=""></img>
+          <img src="/assets/section/3/4.jpg" alt="" className="adjust"></img>
           <h3>老年食用吃海参的建议</h3>
           <p><span></span>海参的补养作用使其成为老年人养生的绝佳选择，但是大部分老年人运化功能趋弱，所以每次半头海参为佳，可以每天食用。</p>
         </div>
         <div className="s3-box">
-        <img src="/assets/section/3/p2.png" alt=""></img>
+          <img src="/assets/section/3/5.jpg" alt=""></img>
           <h3>术后患者最适合食用海参</h3>
           <p><span></span>海参能够帮助术后患者快速恢复元气，恢复期间可以每天早晚个一头，效果显著。</p>
         </div>
